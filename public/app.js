c0099ce431f1f77edadb1b439da65369ce57f244
RicardoJavierFernandez <rickyjf808@gmail.com>
2019-10-19 17:02:47 -0400

    Added mongo db and scraper code

--- public/app.js
+++ public/app.js
@@ -1,1 +1,29 @@
 // Front end logic goes here
+function getArticles() {
+    $('.articles').empty();
+
+    $.ajax({
+        url: '/articles',
+        method: 'GET'
+    }).then((data)=>{
+        for (item of data) {
+            let htmlString = `<div class="title-header" data-id=${item._id}>${item.title}</div>`
+            htmlString += '<div class="title-space"></div>'
+            $('.articles').append(htmlString);
+        }
+    });
+}
+
+
+$(document).ready(getArticles);
+
+
+$('body').on('click', '.title-header', (event) => {
+    // console.log(event.target.dataset.id)
+    $.ajax({
+        url: '/articles/' + event.target.dataset.id,
+        method: 'GET'
+    }).then((response) => {
+        console.log(response);
+    });
+});
